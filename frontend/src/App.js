63c73682beb9881462c8b7e6e3fd86a37acbe189
AayushAlok <59266619+AayushAlok@users.noreply.github.com>
2021-08-04 20:02:42 +0530

    Added homescreen and productscreen routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen';
 
 const App = ()=>{
   return (
-    <>
-    <Header/>
-    <main className="py-3">
-      <Container>
-        <h1>Welcome to Proshop</h1>
-      </Container>
-    </main>
-    <Footer/>
-    </>
+    <Router>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Route path='/' exact component={HomeScreen}/>
+          <Route path='/product/:id' exact component={ProductScreen}/>
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
