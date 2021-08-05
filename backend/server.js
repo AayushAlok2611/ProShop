cd9d0e3e1a24dd9a48f1ea8c4690601f906cb4f5
AayushAlok <59266619+AayushAlok@users.noreply.github.com>
2021-08-05 19:29:36 +0530

    created custom error handler which replaces default express error handler

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,22 @@ app.get('/',(req,res)=>{
     res.send('API is running');
 })
 
+app.use((req,res,next)=>{
+    const error = new Error(`Not found - ${req.originalUrl}`);//throwing a new error if trying to access an undefined
+                                                                //route
+    res.status(404);
+    next(error);
+})
+
+app.use((err,req,res,next)=>{
+    const statusCode = res.statusCode ===200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message:err.message,
+        stack:process.env.NODE_ENV === 'production' ? null :err.stack //stack trace is visible only in development mode
+    })
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
