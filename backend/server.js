f06f291c298816bba78b584d90065bcda8f0a421
AayushAlok <59266619+AayushAlok@users.noreply.github.com>
2021-08-05 19:44:37 +0530

    moved custom error handler to separate file

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import {errorHandler,notFound} from './middleware/errorMiddleware.js';
 
 dotenv.config();
 const app = express();
@@ -18,21 +19,9 @@ app.get('/',(req,res)=>{
     res.send('API is running');
 })
 
-app.use((req,res,next)=>{
-    const error = new Error(`Not found - ${req.originalUrl}`);//throwing a new error if trying to access an undefined
-                                                                //route
-    res.status(404);
-    next(error);
-})
+app.use(notFound);
 
-app.use((err,req,res,next)=>{
-    const statusCode = res.statusCode ===200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-        message:err.message,
-        stack:process.env.NODE_ENV === 'production' ? null :err.stack //stack trace is visible only in development mode
-    })
-})
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
