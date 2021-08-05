6f7f54a0f76b59bced2c4b618c9b679c84d325fb
AayushAlok <59266619+AayushAlok@users.noreply.github.com>
2021-08-05 20:33:17 +0530

    created product actions,action constants and product reducer

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,25 @@
+import {
+    PRODUCT_LIST_FAIL,
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS
+} from '../constants/productConstants'
+import axios from 'axios';
+
+export const listProducts = ()=>async dispatch=>{
+    try {
+        dispatch({type:PRODUCT_LIST_REQUEST});
+        const {data}  = await axios.get('/api/products');
+        dispatch({
+            type:PRODUCT_LIST_SUCCESS,
+            payload:data
+        })
+    } catch (err) {
+        dispatch({
+            type:PRODUCT_LIST_FAIL , 
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        });
+        //Here error.response.data.message is the custom error message , if any , that is thron by our backend server
+        //error.response exists if some error occurs with our backend but if some error occurs with the process
+        //of ending the request to the backend then only error exists no error.response
+    }
+}
---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,30 @@
+import {
+    PRODUCT_LIST_FAIL,
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS
+} from '../constants/productConstants'
+
+const initialState = {
+    products:[],
+    loading:false
+}
+
+export const productListReducer = (state=initialState,action)=>{
+    const {payload,type} = action;
+    switch (type) {
+        case PRODUCT_LIST_REQUEST:
+            return {loading:true,products:[]};
+        case PRODUCT_LIST_SUCCESS:
+            return {
+                loading:false,
+                products:payload
+            };
+        case PRODUCT_LIST_FAIL:
+            return {
+                loading:false,
+                error:payload
+            }
+        default:
+            return state;
+    }
+}
