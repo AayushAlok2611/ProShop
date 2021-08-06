537d11d139a4d8bd91362d2bfdbff6dc6ac950db
AayushAlok <59266619+AayushAlok@users.noreply.github.com>
2021-08-06 10:47:42 +0530

    created action to get singleproduct details

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,7 +1,10 @@
 import {
     PRODUCT_LIST_FAIL,
     PRODUCT_LIST_REQUEST,
-    PRODUCT_LIST_SUCCESS
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
 } from '../constants/productConstants'
 import axios from 'axios';
 
@@ -16,6 +19,26 @@ export const listProducts = ()=>async dispatch=>{
     } catch (error) {
         dispatch({
             type:PRODUCT_LIST_FAIL , 
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        });
+        //Here error.response.data.message is the custom error message , if any , that is thron by our backend server
+        //error.response exists if some error occurs with our backend but if some error occurs with the process
+        //of ending the request to the backend then only error exists no error.response
+    }
+}
+
+export const listProductDetails = (prodId)=>async dispatch=>{
+    try {
+        dispatch({type:PRODUCT_DETAILS_REQUEST});
+
+        const {data}  = await axios.get(`/api/products/${prodId}`);
+        dispatch({
+            type:PRODUCT_DETAILS_SUCCESS,
+            payload:data
+        })
+    } catch (error) {
+        dispatch({
+            type:PRODUCT_DETAILS_FAIL , 
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         });
         //Here error.response.data.message is the custom error message , if any , that is thron by our backend server
