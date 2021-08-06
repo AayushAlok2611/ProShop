ce6d787206ff68c978a059c3305c561158df9b0e
AayushAlok <59266619+AayushAlok@users.noreply.github.com>
2021-08-06 17:12:12 +0530

    corrected default qty of product to be added to cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import {useDispatch,useSelector} from 'react-redux';
+import { addToCart } from '../actions/cartActions';
+import Message from '../components/Message';
 
-const CartScreen = () => {
+const CartScreen = ({match,location,history}) => {
+    const productId = match.params.id; //there is not always an id since in App.js we have stated id as an optional param
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1; //defualt qty is 1
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        if(productId){
+            dispatch(addToCart(productId,qty));
+        }
+    },[dispatch,productId,qty]);
     return (
-        <div>
-            Cart
-        </div>
+        <>
+        </>
     )
 }
 
