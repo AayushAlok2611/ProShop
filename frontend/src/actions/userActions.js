0e5b411fa04b6938609b32754db5b73a68fd70be
AayushAlok <59266619+AayushAlok@users.noreply.github.com>
2021-08-09 19:09:03 +0530

    created action constants , reducer and action to deal with updating user profile

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -9,7 +9,10 @@ import {
   USER_LOGOUT, 
   USER_REGISTER_FAIL, 
   USER_REGISTER_REQUEST, 
-  USER_REGISTER_SUCCESS 
+  USER_REGISTER_SUCCESS, 
+  USER_UPDATE_PROFILE_FAIL, 
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS
 } from "../constants/userConstants"
 
 export const login = (email,password)=>async dispatch=>{
@@ -107,6 +110,41 @@ export const register = (name, email, password) => async (dispatch) => {
       // }
       dispatch({
         type: USER_DETAILS_FAIL,
+        payload: message,
+      })
+    }
+  }
+
+  export const updateUserProfile = (user) => async (dispatch, getState) => { //user object has the new data for user profile
+    try {
+      dispatch({type: USER_UPDATE_PROFILE_REQUEST})
+  
+      const {userLogin: { userInfo }} = getState()
+  
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.put(`/api/users/profile`, user, config)
+  
+      dispatch({
+        type: USER_UPDATE_PROFILE_SUCCESS,
+        payload: data,
+      })
+      localStorage.setItem('userInfo', JSON.stringify(data))
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      // if (message === 'Not authorized, token failed') {
+      //   dispatch(logout())
+      // }
+      dispatch({
+        type: USER_UPDATE_PROFILE_FAIL,
         payload: message,
       })
     }
