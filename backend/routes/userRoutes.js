e43857ae52c902f46d5690178407bf64dc5e3f0d
AayushAlok <59266619+AayushAlok@users.noreply.github.com>
2021-08-09 18:32:08 +0530

    created route and controller to update user profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,8 @@ import express from 'express';
 import { 
     authUser,
     getUserProfile,
-    registerUser 
+    registerUser, 
+    updateUserProfile
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
@@ -12,6 +13,7 @@ router.route('/').post(registerUser);
 
 router.route('/login').post(authUser);
 
-router.route('/profile').get(protect,getUserProfile);
+router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile); //GET AND PUT REQUEST TO SAME ENDPOINT
+
 
 export default router;
